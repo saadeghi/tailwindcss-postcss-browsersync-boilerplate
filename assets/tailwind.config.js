b68b41bdd776bcca0e5dd615965f5005194711a1
Pouya <pouya.saadeghi@gmail.com>
2020-04-29 22:40:12 +0430

    init

---
+++ assets/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['Kanit', 'sans-serif'],
+    },
+    colors: {
+      "primary": "#dc4482",
+    },
+    extend: {
+    }
+  },
+  variants: {
+  },
+  plugins: [],
+}
